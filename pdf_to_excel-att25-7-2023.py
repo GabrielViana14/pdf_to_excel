cb44427c9b7b8457d92f8f5c220c93980a88bbab
GabrielViana14 <130351936+GabrielViana14@users.noreply.github.com>
2023-07-25 17:00:19 -0300

    Add files via upload

---
+++ pdf_to_excel-att25-7-2023.py
@@ -0,0 +1,49 @@
+import pdftables_api
+import pandas as pd
+import os
+from os import system
+import warnings
+import numpy as np
+warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+
+#pdf para excel
+if os.path.isfile('Report.xlsx'):
+    print("Arquivo já existente")
+    resp = input("Deseja sobreescrever o mesmo?")
+    if resp.lower() in ["s", "sim"]:
+        c = pdftables_api.Client('0h5a1mj2w18n')
+        c.xlsx('Report.pdf', 'Report')   
+    else:
+        print("Arquivo criado")
+
+
+#excel 
+arq_excel = "Report.xlsx"
+dataframes = pd.read_excel(arq_excel,sheet_name=None)
+dados_combinados = pd.concat(dataframes.values(),ignore_index=True)
+df= pd.DataFrame(dados_combinados)
+remove = [" ̈","1/1","No desenho","Itens Rateio","MP","Máquina","Tp. Serviço",'MSM CALDEIRARIA - www.msmcaldeiraria.com.br','CNPJ.: 06183455000176 IE.: 581.067.044.113']
+for frase in remove:
+    df.replace(frase,np.nan,regex=True,inplace=True)
+coldel= [1,2,5]
+df=df.drop(df.columns[coldel],axis=1)
+df=df.dropna(how="all")
+df.apply(lambda x: x.dropna().reset_index(drop=True), axis=1)
+
+df= df.apply(lambda x: pd.Series(x.dropna().values),axis=1)
+
+idx_apelido = df[df[1] == 'Apelido:'].index[0]
+idx_producao = df[df[5] == 'PRODUÇÃO'].index[0]
+df.loc[idx_apelido:idx_producao,1:5] = ''
+df.reset_index(drop=True, inplace=True)
+dados_combinados = df
+dados_combinados.to_excel("Report_att.xlsx",index=False,engine="openpyxl")
+
+
+print(dados_combinados)
+print("Report_att.xslx foi atualizado")
+nomearq = "Report_att.xlsx"
+if os.path.exists(nomearq):
+    os.system(f'start excel "{nomearq}"')
+else:
+    print("Arquivo não encontrado.")
